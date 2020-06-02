b0c61c6e642525102f7bc215f787cf7348553249
Alogenesis <nononosolo@hotmail.com>
2020-06-02 19:10:03 +0700

    Initial commit

---
+++ Interrepeater/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\User Data\Documents\GitHub\บัตรคิวออนไลน์\Interrepeater\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
