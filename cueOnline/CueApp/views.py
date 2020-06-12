4b16cd03307b07b0563249d8953b2fea9ef6ecf2
Alogenesis <nononosolo@hotmail.com>
2020-06-13 00:25:17 +0700

    Add User register condition

--- cueOnline/CueApp/views.py
+++ cueOnline/CueApp/views.py
@@ -11,5 +11,37 @@ def info(request):
 def register(request):
     return render(request, 'register.html')
 
+def addUser(request):
+    username = request.POST['username']
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    email = request.POST['email']
+    password = request.POST['password']
+    repassword = request.POST['repassword']
+
+    if password == repassword : #เช็คว่า password 2 ช่องตรงกันมั้ย ถ้าตรงไปต่อ
+        if User.objects.filter(username=username).exists(): #เช็คว่า username ซ้ำกับที่เคยลงไว้มั้ย
+            messages.info(request,'Username นี้มีผู้ใช้แล้ว')
+            print("Username นี้มีคนใช้แล้ว")
+            return redirect('/register')
+        elif User.objects.filter(email=email).exists(): #เช็คว่า email ซ้ำมั้ย
+            messages.info(request, 'Email นี้มีผู้ใช้แล้ว')
+            print("Email นี้มีผู้ใช้งานแล้ว")
+            return redirect('/register')
+        else:   #ถ้าไม่ซ้ำ ให้ทำการบันทึกข้อมูล
+            User.objects.create_user(
+            username = username,
+            password = password,
+            email = email,
+            first_name = firstname,
+            last_name = lastname,
+            )
+            User.save()
+            return redirect('/')
+    else:   #password ไม่ตรงกัน ลงทะเบียนใหม่
+        messages.info(request, 'Password ไม่ตรงกัน')
+        return redirect('/register')
+
+
 def register_done(request):
     return render(request, 'register_done.html')
