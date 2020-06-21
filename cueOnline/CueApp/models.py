80e032e683f9f0e7a5a2fc7a4aa900d48b1b57e4
Alogenesis <nononosolo@hotmail.com>
2020-06-21 23:07:08 +0700

    Create Model for Pressing Cue A B C

--- cueOnline/CueApp/models.py
+++ cueOnline/CueApp/models.py
@@ -10,4 +10,13 @@ class Cue(models.Model):
     cue_type = models.CharField(max_length=60)
     cue_number = models.IntegerField()
 
+class Press_cue_A(models.Model):
+    cue_type = models.CharField(max_length=60)
+
+class Press_cue_B(models.Model):
+    cue_type = models.CharField(max_length=60)
+
+class Press_cue_C(models.Model):
+    cue_type = models.CharField(max_length=60)
+
 
